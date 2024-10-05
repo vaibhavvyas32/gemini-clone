9fef8a59c4a9a96bcc16034c47fa115faa4638f9
vaibhavvyas32 <vaibhavvyas32@gmail.com>
2024-10-05 16:04:06 +0530

    added complete ui part

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,11 +1,54 @@
 import React from 'react'
 import './Main.css'
+import { assets } from '../../assets/assets'
 
 type Props = {}
 
 const Main = (props: Props) => {
     return (
-        <div>Main</div>
+        <div className='main'>
+            <div className="nav">
+                <p>Gemini</p>
+                <img src={assets.user_icon} alt="" />
+            </div>
+            <div className="main-container">
+                <div className="greet">
+                    <p><span>Hello, Genx</span></p>
+                    <p>How can I help you today?</p>
+                </div>
+                <div className="cards">
+                    <div className="card">
+                        <p>Suggest something somethingsomething something</p>
+                        <img src={assets.compass_icon} alt="" />
+                    </div>
+                    <div className="card">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis quod alias, illo eveniet ae aliquam vero accusantium! Ad, minima sed!</p>
+                        <img src={assets.bulb_icon} alt="" />
+                    </div>
+                    <div className="card">
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illoibusdam odit illum eveniet accusantium quia. Voluptatem, expedita aliquam!</p>
+                        <img src={assets.message_icon} alt="" />
+                    </div>
+                    <div className="card">
+                        <p>Lorem ipsum dolor sit amet,  veniam  optio unde exercitationem, suscipit ex voluptatem ad! Fugiat qui eum eius?</p>
+                        <img src={assets.code_icon} alt="" />
+                    </div>
+                </div>
+                <div className="main-bottom">
+                    <div className="search-box">
+                        <input type="text" placeholder='Enter a prompt here' />
+                        <div>
+                            <img src={assets.gallery_icon} alt="" />
+                            <img src={assets.mic_icon} alt="" />
+                            <img src={assets.send_icon} alt="" />
+                        </div>
+                    </div>
+                    <p className="bottom-info">
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aperiam voluptatum laborum dicta
+                    </p>
+                </div>
+            </div>
+        </div>
     )
 }
 
