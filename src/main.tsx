ff4a024707a4d33e8b8c4d36d98312bb0f989e8f
vaibhavvyas32 <vaibhavvyas32@gmail.com>
2024-10-07 16:08:40 +0530

    add gemini api logic, new chat logic, prompt logic...

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import ContextProvider from './context/Context.js'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  <ContextProvider>
     <App />
-  </StrictMode>,
+  </ContextProvider>,
 )
